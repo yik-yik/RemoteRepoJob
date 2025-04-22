a6c6d8a7266e8e59364f93486b6a52f8d5689268
Yiks <yikyik.beh@natwest.com>
2025-04-22 12:26:55 +0100

    new version

--- Program.java
+++ Program.java
@@ -1,6 +1,8 @@
 import java.io.*;
 public class Program{
-  public static void main(String[] args){
-  System.out.println("Hello User");
-  }
+public static void main(String[] args){
+for(String str : args){
+System.out.println("Hello " + str);
+}
+}
 }
